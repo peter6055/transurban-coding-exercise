6e38677ef5f0932d46899e7cb1fcb1573d99d47d
peter6055 <59694246+peter6055@users.noreply.github.com>
2024-09-19 19:29:16 +1000

    CDK Stack: setup dynamodb table, lambda, api gateway config

--- lib/transurban-coding-exercise-stack.ts
+++ lib/transurban-coding-exercise-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {AttributeType, Table} from 'aws-cdk-lib/aws-dynamodb';
+import {ApiKey, ApiKeySourceType, Cors, RestApi} from "aws-cdk-lib/aws-apigateway";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 
 
 export class TransurbanCodingExerciseStack extends cdk.Stack {
@@ -12,6 +15,37 @@ export class TransurbanCodingExerciseStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'TransurbanCodingExerciseQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // create a dynamodb table
+    const addressTable = new Table(this, 'address', {
+        partitionKey: { name: 'userId', type: AttributeType.STRING },
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+
+    // create a lambda function
+    const addressLambda = new NodejsFunction(this, 'PostsLambda', {
+      entry: 'resources/endpoints/address.ts',
+      handler: 'handler',
+      environment: {
+        TABLE_NAME: addressTable.tableName,
+      },
+    });
+
+    // create an api gateway
+    const api = new RestApi(this, 'RestAPI', {
+      restApiName: 'RestAPI',
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+      apiKeySourceType: ApiKeySourceType.HEADER,
+    });
+
+    // create an api key
+    const apiKey = api.addApiKey('ApiKey');
+
+
 
   }
 }
