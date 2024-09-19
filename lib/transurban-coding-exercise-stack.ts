d2c12b9857dd0305062bdc7c3b47093ef95dffb0
peter6055 <59694246+peter6055@users.noreply.github.com>
2024-09-20 00:42:32 +1000

    Bug Fixed: Introduce GSI into table and change the partitionKey to ensure the search efficiency and functionality

--- lib/transurban-coding-exercise-stack.ts
+++ lib/transurban-coding-exercise-stack.ts
@@ -1,78 +1,87 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import {AttributeType, Table} from 'aws-cdk-lib/aws-dynamodb';
+import {Construct} from 'constructs';
+import {AttributeType, TableV2} from 'aws-cdk-lib/aws-dynamodb';
 import {ApiKeySourceType, Cors, LambdaIntegration, RestApi, UsagePlan} from "aws-cdk-lib/aws-apigateway";
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import {CfnOutput} from "aws-cdk-lib";
 
 
 export class TransurbanCodingExerciseStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // create a dynamodb table
-    const addressTable = new Table(this, 'address', {
-        partitionKey: { name: 'id', type: AttributeType.STRING },
-        removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-
-
-    // create an api gateway
-    const api = new RestApi(this, 'RestAPI', {
-      restApiName: 'TransurbanCodingExerciseAPI',
-      defaultCorsPreflightOptions: {
-        allowOrigins: Cors.ALL_ORIGINS,
-        allowMethods: Cors.ALL_METHODS,
-      },
-      apiKeySourceType: ApiKeySourceType.HEADER,
-    });
-
-    // create a usage plan
-    const plan = api.addUsagePlan('UsagePlan', {
-      name: 'Usage Plan',
-      apiStages: [
-        {
-          api,
-          stage: api.deploymentStage,
-        }
-      ],
-    });
-
-    // create an api key for testing purposes
-    const apiKey = api.addApiKey('ApiKey');
-    plan.addApiKey(apiKey);
+        // create a dynamodb table
+        const addressTable = new TableV2(this, 'address', {
+            partitionKey: {name: 'userId', type: AttributeType.STRING},
+            sortKey: {name: 'id', type: AttributeType.STRING},
+            globalSecondaryIndexes: [
+                {
+                    indexName: 'suburb',
+                    partitionKey: {name: 'pk', type: AttributeType.STRING},
+                },
+                {
+                  indexName: 'postcode',
+                  partitionKey: {name: 'pk', type: AttributeType.STRING},
+                },
+            ],
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
 
 
+        // create an api gateway
+        const api = new RestApi(this, 'RestAPI', {
+            restApiName: 'TransurbanCodingExerciseAPI',
+            defaultCorsPreflightOptions: {
+                allowOrigins: Cors.ALL_ORIGINS,
+                allowMethods: Cors.ALL_METHODS,
+            },
+            apiKeySourceType: ApiKeySourceType.HEADER,
+        });
 
-    // create a lambda function
-    const addressLambda = new NodejsFunction(this, 'addressLambda', {
-      entry: 'resources/endpoints/address.ts',
-      handler: 'handler',
-      environment: {
-        TABLE_NAME: addressTable.tableName,
-      },
-    });
-    addressTable.grantReadWriteData(addressLambda);
+        // create a usage plan
+        const plan = api.addUsagePlan('UsagePlan', {
+            name: 'Usage Plan',
+            apiStages: [
+                {
+                    api,
+                    stage: api.deploymentStage,
+                }
+            ],
+        });
 
-
-    // create the api gateway resources and attach it to the lambda function
-    const posts = api.root.addResource('address');
-
-    posts.addResource('create').addMethod('POST', new LambdaIntegration(addressLambda), {
-      apiKeyRequired: true,
-    });
-
-    posts.addResource('find').addMethod('POST', new LambdaIntegration(addressLambda), {
-      apiKeyRequired: true,
-    });
-
-
-
-    // output api key
-    new CfnOutput(this, 'API Key ID', {
-      value: apiKey.keyId,
-    });
+        // create an api key for testing purposes
+        const apiKey = api.addApiKey('ApiKey');
+        plan.addApiKey(apiKey);
 
 
-  }
+        // create a lambda function
+        const addressLambda = new NodejsFunction(this, 'addressLambda', {
+            entry: 'resources/endpoints/address.ts',
+            handler: 'handler',
+            environment: {
+                TABLE_NAME: addressTable.tableName,
+            },
+        });
+        addressTable.grantReadWriteData(addressLambda);
+
+
+        // create the api gateway resources and attach it to the lambda function
+        const posts = api.root.addResource('address');
+
+        posts.addResource('create').addMethod('POST', new LambdaIntegration(addressLambda), {
+            apiKeyRequired: true,
+        });
+
+        posts.addResource('find').addMethod('POST', new LambdaIntegration(addressLambda), {
+            apiKeyRequired: true,
+        });
+
+
+        // output api key
+        new CfnOutput(this, 'API Key ID', {
+            value: apiKey.keyId,
+        });
+
+
+    }
 }
