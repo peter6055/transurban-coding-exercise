58615777adf8823c0b1df914cb79ce19d2355b09
peter6055 <59694246+peter6055@users.noreply.github.com>
2024-09-20 00:46:52 +1000

    Bug Fixed: *Missing address attribute will cause 500 error. *Missing suburb identification

--- resources/handlers/address/create.ts
+++ resources/handlers/address/create.ts
@@ -19,7 +19,14 @@ export async function create(body: string | null) {
 
     // Parse the body
     const bodyParsed = JSON.parse(body) as Body;
-    if(!bodyParsed.userId || !bodyParsed.address.line || !bodyParsed.address.line || !bodyParsed.address.state || !bodyParsed.address.postcode){
+    if(
+        !bodyParsed.userId ||
+        !bodyParsed.address ||
+        !bodyParsed.address.line ||
+        !bodyParsed.address.suburb ||
+        !bodyParsed.address.state ||
+        !bodyParsed.address.postcode
+    ){
         return {
             statusCode: 400,
             body: JSON.stringify({ message: 'Missing necessary request body' }),
