b0bd95c233aa0e8fef83b1f5dd1955e85f716180
peter6055 <59694246+peter6055@users.noreply.github.com>
2024-09-19 22:51:08 +1000

    Feature: endpoint `/address/create` release, allow user to create address

---
+++ resources/handlers/address/create.ts
@@ -0,0 +1,47 @@
+import {uuid}  from 'uuidv4';
+import {PutCommand} from "@aws-sdk/lib-dynamodb";
+import {DynamoDB} from "@aws-sdk/client-dynamodb";
+import {Address, UserId} from "../../../types";
+
+type Body = {
+    userId: UserId,
+    address: Address
+}
+
+export async function create(body: string | null) {
+    if (!body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing request body' }),
+        };
+    }
+
+
+    // Parse the body
+    const bodyParsed = JSON.parse(body) as Body;
+    if(!bodyParsed.userId || !bodyParsed.address.line || !bodyParsed.address.line || !bodyParsed.address.state || !bodyParsed.address.postcode){
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing necessary request body' }),
+        };
+    }
+
+
+    // Creat the address
+    const dynamodb = new DynamoDB({});
+    await dynamodb.send(
+        new PutCommand({
+            TableName: process.env.TABLE_NAME,
+            Item: {
+                id: `${uuid()}`,
+                userId: bodyParsed.userId,
+                ...bodyParsed.address,
+            },
+        })
+    );
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Address Created!' }),
+    };
+}
