6297d6a2762d56bf2614f3cd98d462d448eefc15
peter6055 <59694246+peter6055@users.noreply.github.com>
2024-09-20 20:29:51 +1000

    Adding CDK template test

---
+++ test/transurban-coding-exercise.test.ts
@@ -0,0 +1,127 @@
+import * as cdk from 'aws-cdk-lib';
+import {Capture, Template} from 'aws-cdk-lib/assertions';
+import * as TransurbanCodingExercise from '../lib/transurban-coding-exercise-stack';
+
+const app = new cdk.App();
+const stack = new TransurbanCodingExercise.TransurbanCodingExerciseStack(app, 'TestStack');
+const template = Template.fromStack(stack);
+
+
+test('Address DynamoDB template', () => {
+    template.hasResourceProperties("AWS::DynamoDB::GlobalTable", {
+        TableName: "TU_DDB_Table_Address",
+        KeySchema: [
+            {
+                "AttributeName": "userId",
+                "KeyType": "HASH"
+            },
+            {
+                "AttributeName": "id",
+                "KeyType": "RANGE"
+            }
+        ],
+        GlobalSecondaryIndexes: [
+            {
+                "IndexName": "suburb",
+                "KeySchema": [
+                    {
+                        "AttributeName": "pk",
+                        "KeyType": "HASH"
+                    }
+                ],
+                "Projection": {
+                    "ProjectionType": "ALL"
+                }
+            },
+            {
+                "IndexName": "postcode",
+                "KeySchema": [
+                    {
+                        "AttributeName": "pk",
+                        "KeyType": "HASH"
+                    }
+                ],
+                "Projection": {
+                    "ProjectionType": "ALL"
+                }
+            }
+        ],
+    });
+});
+
+
+test('API Gateway template', () => {
+    template.hasResourceProperties("AWS::ApiGateway::RestApi", {
+        ApiKeySourceType: "HEADER",
+        Name: "TU_API_Gateway"
+    });
+});
+
+test('API Usage Plan template', () => {
+    // RestApiId is a dynamic value, so we need to capture it
+    const restApiIdCapture = new Capture();
+
+    template.hasResourceProperties("AWS::ApiGateway::UsagePlan", {
+        UsagePlanName: "TU_API_UsagePlan",
+        ApiStages: [
+            {
+                ApiId: {
+                    "Ref": restApiIdCapture
+                }
+            }
+        ]
+    });
+
+    // Check that the RestApiId is a string that contains "TU_API_Gateway", which is the name of the api gateway
+    expect(restApiIdCapture.asString()).toContain(`TUAPIGateway`);
+});
+
+
+test('API Key template', () => {
+    // RestApiId is a dynamic value, so we need to capture it
+    const restApiIdCapture = new Capture();
+
+    template.hasResourceProperties("AWS::ApiGateway::ApiKey", {
+        Enabled: true,
+        StageKeys: [
+            {
+                RestApiId: {
+                    "Ref": restApiIdCapture
+                }
+            }
+        ]
+    });
+
+    // Check that the RestApiId is a string that contains "TU_API_Gateway", which is the name of the api gateway
+    expect(restApiIdCapture.asString()).toContain(`TUAPIGateway`);
+});
+
+
+test('Lambda template', () => {
+    // tableNameCapture is a dynamic value, so we need to capture it
+    const tableNameCapture = new Capture();
+
+    template.hasResourceProperties("AWS::Lambda::Function", {
+        Handler: "index.handler",
+        Environment: {
+            Variables: {
+                TABLE_NAME: {
+                    Ref: tableNameCapture
+                }
+            }
+        },
+    });
+
+    // Check that the RestApiId is a string that contains "TU_API_Gateway", which is the name of the api gateway
+    expect(tableNameCapture.asString()).toContain(`TUDDBTableAddress`);
+});
+
+
+
+/*
+ * The following tests are not covered
+ *  - API Gateway method (`api.root.addResource`, `address.addResource`), since the lambda function is dynamically created
+ *    recommend to do it on integration testing
+ *  - Output API Endpoint URL and Key ID, since it is a dynamic value
+ *
+ */
